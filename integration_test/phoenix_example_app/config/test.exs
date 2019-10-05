983b5342cc4cc346581c53dd3ccc5b5bf90f2185
Eduard Litau <eduard.litau@gmail.com>
2019-10-06 01:20:59 +0200

    Fixed execution of tests on my maschine

--- integration_test/phoenix_example_app/config/test.exs
+++ integration_test/phoenix_example_app/config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # Configure your database
 config :phoenix_example_app, PhoenixExampleApp.Repo,
-  username: "postgres",
+  username: System.get_env("PG_USER") || System.get_env("USER") || "postgres",
   password: "postgres",
   database: "phoenix_example_app_test",
   hostname: "localhost",
