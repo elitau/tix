7277ffc9118f073fb9ca5672c0a31d5584472f6c
Eduard Litau <eduard.litau@gmail.com>
2019-10-09 00:59:28 +0200

    Focus on a single test with the special comment: tix:focus

--- lib/tix/selector.ex
+++ lib/tix/selector.ex
@@ -23,6 +23,38 @@ defmodule Tix.Selector do
     File.exists?(path)
   end
 
+  defmodule FocusComment do
+    def matching(path) do
+      case File.read(path) do
+        {:ok, content} ->
+          String.match?(content, ~r/#.*tix.*focus/) |> path_for_focus_comment(path, content)
+
+        {:error, _posix_error} ->
+          nil
+      end
+    end
+
+    defp path_for_focus_comment(false, _path, _content) do
+      nil
+    end
+
+    defp path_for_focus_comment(true, path, content) do
+      line_number =
+        String.split(content, "\n")
+        |> Enum.find_index(&String.match?(&1, ~r/^.*#.*tix.*focus.*$/))
+
+      case line_number do
+        nil ->
+          nil
+
+        line_number ->
+          # add 1 because the index is zero based
+          # add another 1 because the test is one line below the comment
+          {path, line_number + 2}
+      end
+    end
+  end
+
   def possible_test_file_paths(nil), do: []
   def possible_test_file_paths(""), do: []
   def possible_test_file_paths([]), do: []
@@ -30,6 +62,7 @@ defmodule Tix.Selector do
   def possible_test_file_paths(path) when is_binary(path) do
     [
       Tix.PinnedTest.pinned_test(),
+      FocusComment.matching(path),
       {path, nil},
       {nearby_path(path), nil},
       {test_folder_path(path), nil}
