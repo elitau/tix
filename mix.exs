d016e0efe393714b3b6f9b8b40482ea583644be3
Eduard Litau <eduard.litau@gmail.com>
2020-06-29 12:11:43 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tix.MixProject do
   def project do
     [
       app: :tix,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
