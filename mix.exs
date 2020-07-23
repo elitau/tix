52da362593bfa72aff3fe6ea11e57918ca34e9d3
Eduard Litau <eduard.litau@gmail.com>
2020-07-23 21:25:28 +0200

    just to be up to date

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tix.MixProject do
     [
       app: :tix,
       version: "0.4.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
