af140159762bf808748fe44a20037184457e51fa
Eduard Litau <eduard.litau@gmail.com>
2022-03-17 00:18:22 +0100

    Less indirect file watcher
    To aid testing of tix:
    Allow to parameterize test runner

--- lib/tix/file_watcher.ex
+++ lib/tix/file_watcher.ex
@@ -4,25 +4,23 @@ defmodule Tix.FileWatcher do
   triggers commands and notifications
   """
   require Logger
-  defstruct changed_files: []
+  defstruct runner: :not_set
+  @default_test_runner &Tix.TestRunner.execute_test/1
 
   use GenServer
 
   @doc false
-  def start_link([]), do: start_link()
-
-  def start_link do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   @doc false
-  def init(_) do
-    :erlang.send_after(1000, self(), :handle_file_changes)
+  def init(opts \\ []) do
     {:ok, path} = File.cwd()
     watch_with_file_system(path)
     IO.puts("Tix is watching your files at #{path}")
     TestIex.start_testing()
-    {:ok, %__MODULE__{}}
+    {:ok, %__MODULE__{runner: Keyword.get(opts, :runner, @default_test_runner)}}
   end
 
   defp watch_with_file_system(path) do
@@ -34,36 +32,11 @@ defmodule Tix.FileWatcher do
   Executes on file changes with fs
   """
   def handle_info({:file_event, _watcher_pid, {path, _events}}, state) do
-    matched? = path |> to_string |> path_match?(~r{\.(ex|exs|eex)\z}i)
-
-    changed_files =
-      if matched? do
-        Enum.uniq(state.changed_files ++ [to_string(path)])
-      else
-        state.changed_files
-      end
-
-    {:noreply, %{state | changed_files: changed_files}}
-  end
-
-  @doc "Receives list of changed files and executes tests. Resets changed files state"
-  def handle_info(:handle_file_changes, state) do
-    case state.changed_files do
-      [] ->
-        :noop
-
-      [file] ->
-        file
-        |> Tix.Focus.scan_for_focused_test()
-        |> Tix.Selector.tests_for_file()
-        |> execute_tests()
-
-      many_files ->
-        Tix.debug("Many files changed: #{many_files |> inspect()}. Will NOT execute tests")
+    if valid_file?(path) do
+      path |> to_string() |> handle_file_change(state)
     end
 
-    :erlang.send_after(500, self(), :handle_file_changes)
-    {:noreply, %{state | changed_files: []}}
+    {:noreply, state}
   end
 
   @doc false
@@ -71,18 +44,28 @@ defmodule Tix.FileWatcher do
     {:noreply, %{state | changed_files: []}}
   end
 
+  defp handle_file_change(file, state) do
+    file
+    |> Tix.Focus.scan_for_focused_test()
+    |> Tix.Selector.tests_for_file()
+    |> execute_tests(state)
+  end
+
   @doc false
-  def execute_tests(changed_files) do
+  def execute_tests(files_to_execute, %{runner: runner}) do
     try do
       IEx.Helpers.recompile()
 
-      changed_files
-      |> Tix.TestRunner.execute_test()
+      runner.(files_to_execute)
     rescue
       e ->
         e |> Exception.message() |> IO.inspect()
         __STACKTRACE__ |> Exception.format_stacktrace() |> IO.puts()
     end
+  end
+
+  defp valid_file?(path) do
+    path |> to_string |> path_match?(~r{\.(ex|exs|eex)\z}i)
   end
 
   defp path_match?(path, pattern = %Regex{}) do
